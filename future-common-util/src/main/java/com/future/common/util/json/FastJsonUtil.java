44d0529b9ff3112bf18bb420da71fbf46313a419
future <yaoguang.wang@aihuishou.com>
2021-05-04 23:53:09 +0800

    json

--- future-common-util/src/main/java/com/future/common/util/json/FastJsonUtil.java
+++ future-common-util/src/main/java/com/future/common/util/json/FastJsonUtil.java
@@ -3,9 +3,13 @@ package com.future.common.util.json;
 import com.alibaba.fastjson.JSON;
 
 /**
+ * 推荐使用:http://yaoguang.info/2020/06/15/java%E5%B8%B8%E7%94%A8JSON%E5%BA%93/
+ * @see JacksonUtil
+ *
  * @author future
  * @date 2021/05/04 23:41
  */
+@Deprecated
 public class FastJsonUtil {
 
     public static String beanToJson(Object obj) {
--- future-common-util/src/main/java/com/future/common/util/json/GsonUtil.java
+++ future-common-util/src/main/java/com/future/common/util/json/GsonUtil.java
@@ -4,9 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
+ * 推荐使用:http://yaoguang.info/2020/06/15/java%E5%B8%B8%E7%94%A8JSON%E5%BA%93/
+ * @see JacksonUtil
+ *
  * @author future
  * @date 2021/05/04 23:36
  */
+@Deprecated
 public class GsonUtil {
 
     private static final Gson GSON = new GsonBuilder().create();
