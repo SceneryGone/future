3da1610dba632baf5fe7bc2fb49c3c2083ac3f08
future <yaoguang.wang@aihuishou.com>
2021-05-06 11:37:42 +0800

    mybatis

--- future-mybatis/src/main/java/com/future/mybatis/CommonMapper.java
+++ future-mybatis/src/main/java/com/future/mybatis/CommonMapper.java
@@ -5,6 +5,8 @@ import tk.mybatis.mapper.common.BaseMapper;
 import tk.mybatis.mapper.common.ids.SelectByIdsMapper;
 
 /**
+ * 通用类
+ *
  * @author future
  * @date 2021/05/06 10:16
  */
--- future-swagger-starter/src/main/java/com/future/swagger/SwaggerConfiguration.java
+++ future-swagger-starter/src/main/java/com/future/swagger/SwaggerConfiguration.java
@@ -1,6 +1,5 @@
 package com.future.swagger;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
